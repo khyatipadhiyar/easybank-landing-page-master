1296ff21c4dd6af32e2e692390921bb61f7a0fa4
unknown <khyati11892@gmail.com>
2020-03-13 11:07:28 +0530

    'six'

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // const hamburger = document.querySelector('#hamburger');
  const navmenu = document.querySelector('.navMenu');
-// const  intro = document.querySelector('.intro-img');
+const  intro = document.querySelector('.overlay-intro');
 // hamburger.addEventListener('click',() => {
 // 	navmenu.classList.toggle('show');
 	
@@ -8,6 +8,6 @@
 
 function myFunction(x) {
   x.classList.toggle("change");
-    // intro.classList.toggle('overlay');
+    intro.classList.toggle('overlay');
     navMenu.classList.toggle('show');
 }
